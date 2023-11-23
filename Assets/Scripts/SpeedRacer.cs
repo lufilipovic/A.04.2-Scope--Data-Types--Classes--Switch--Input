584c7bd60faa0119ce5b4130af33ccf1356aa14d
Lucija Filipovic <lucija_filipovic_@hotmail.com>
2023-11-23 13:57:00 +0200

    Adding text in Unity to show information about the car

--- Assets/Scripts/SpeedRacer.cs
+++ Assets/Scripts/SpeedRacer.cs
@@ -17,28 +17,41 @@ public class SpeedRacer : MonoBehaviour
     public bool isCarTypeSedan = false;
     public bool hasFrontEngine = true;
 
-    public TextMeshProUGUI textUI;
+    public TextMeshProUGUI fuelConsText;
+    public TextMeshProUGUI carInfoText;
+    public TextMeshProUGUI carWeightText;
+    public TextMeshProUGUI yearText;
+    public TextMeshProUGUI frontEngineText;
 
     void Start()
     {
         print("The racer model is " + carModel + ". Car maker is " + carMaker + ". It has a " + engineType + " engine.");
+        carInfoText.text = "The racer model is " + carModel + ". Car maker is " + carMaker + ". It has a " + engineType + " engine.";
 
         CheckWeight();
 
         if (yearMade <= 2009)
         {
             print("It was first introduced in " + yearMade);
+            yearText.text = "It was first introduced in " + yearMade;
+
             int carAge = CalculateAge(yearMade);
+
             print("That makes it " + carAge + " years old.");
+            yearText.text = "That makes it " + carAge + " years old.";
 
         }
         else
         {
             print("The car was introduced in the 2010's.");
+            yearText.text = "The car was introduced in the 2010's.";
             print(carModel + " has a maximum acceleration of " + maxAcceleration);
+            yearText.text = carModel + " has a maximum acceleration of " + maxAcceleration;
         }
 
         print(CheckCharacteristics());
+
+        frontEngineText.text = CheckCharacteristics();
     }
 
     void Update()
@@ -73,18 +86,19 @@ public class SpeedRacer : MonoBehaviour
         {
             case 70:
                 print("fuel level is nearing two-thirds.");
-                textUI.text = "fuel level is nearing two-thirds.";
+                fuelConsText.text = "fuel level is nearing two-thirds.";
                 break;
             case 50:
                 print("fuel level is at half amount.");
-                textUI.text = "fuel level is at half amount.";
+                fuelConsText.text = "fuel level is at half amount.";
                 break;
             case 10:
                 print("Warning! Fuel level is critically low.");
-                textUI.text = "Warning! Fuel level is critically low..";
+                fuelConsText.text = "Warning! Fuel level is critically low..";
                 break;
             default:
                 print("Nothing to report.");
+                fuelConsText.text = "Nothing to report.";
                 break;
         }
     }
@@ -96,10 +110,12 @@ public class SpeedRacer : MonoBehaviour
         if(carWeight < 1500)
         {
             print(carModel + " weights less than 1500 kg.");
+            carWeightText.text = carModel + " weights less than 1500 kg.";
         }
         else
         {
             print(carModel + " weights over 1500 kg.");
+            carWeightText.text = carModel + " weights over 1500 kg.";
         }
     }
 
